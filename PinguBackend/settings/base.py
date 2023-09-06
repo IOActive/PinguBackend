c3f92fa2237de0ababacf6cb02334bef52b0043e
doopel <doopel@protonmail.com>
2023-09-06 13:35:11 +0200

    Adding Server site pagination

--- PinguBackend/settings/base.py
+++ PinguBackend/settings/base.py
@@ -82,7 +82,10 @@ REST_FRAMEWORK = {
     
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
-    ]
+    ],
+    
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
 }
 
 CORS_ORIGIN_ALLOW_ALL = False
