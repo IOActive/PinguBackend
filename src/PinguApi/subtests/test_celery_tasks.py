4dfe81ea2a6967b5b2e6fdf716cadeb1366832c5
doopel <doopel@protonmail.com>
2024-03-25 14:13:05 +0100

    adding coopyright header

--- src/PinguApi/subtests/test_celery_tasks.py
+++ src/PinguApi/subtests/test_celery_tasks.py
@@ -1,3 +1,17 @@
+# Copyright 2024 IOActive
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from PinguApi.utils.MinioManager import MinioManger
 from PinguApi.tasks import upload_fuzzer_to_bucket, download_fuzzer_from_bucket, remove_fuzzer_from_bucket, download_bot_logs
 from minio.helpers import ObjectWriteResult
